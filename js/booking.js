5397306b40c26b58b8e4ad1f8f6573e1d28166c6
emmanueluwa <oemmanuel.uwadiae@gmail.com>
2026-02-20 12:21:14 +0000

    deploy to s3 with deployed backend

--- js/booking.js
+++ js/booking.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8000";
+import { API_URL } from "./config.js";
 
 let selectedSlot = null;
 
---
+++ js/config.example.js
@@ -0,0 +1,2 @@
+const API_URL = "http://your-api-url-here.elasticbeanstalk.com";
+export { API_URL };
